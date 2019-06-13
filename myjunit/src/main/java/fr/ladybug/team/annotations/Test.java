654c15b6023cf38532766fe0645d7653838950bb
Ivan Shershnev <ivankrut856@yandex.ru>
2019-06-13 12:47:33 +0300

    docs added

--- myjunit/src/main/java/fr/ladybug/team/annotations/Test.java
+++ myjunit/src/main/java/fr/ladybug/team/annotations/Test.java
@@ -9,6 +9,16 @@ import java.lang.annotation.Target;
 @Target(value= ElementType.METHOD)
 @Retention(value= RetentionPolicy.RUNTIME)
 public @interface Test {
+    /**
+     * Gives the array of the Classes of the exceptions one of which must be thrown during method invocation.
+     * Or empty array if no such exceptions
+     * @return the Classes of the exception to be thrown or empty array
+     */
     Class<?>[] expected() default {};
+
+    /**
+     * Returns empty array if the method must not be ignored or single element array with a message with the reason for ignore
+     * @return single element array with a message with the reason for ignore of empty array if no such reason
+     */
     String[] ignore() default {};
 }
